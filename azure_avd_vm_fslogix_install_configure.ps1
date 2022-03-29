2a033cd3ba719c3304ac5497701db7a232ddfd9f
Kent Taylor <r.t.kentoo@gmail.com>
2022-03-29 14:01:58 -0700

    Update azure_avd_vm_fslogix_install_configure.ps1

--- azure_avd_vm_fslogix_install_configure.ps1
+++ azure_avd_vm_fslogix_install_configure.ps1
@@ -8,5 +8,5 @@ $assignedUser = (get-azwvdsessionhost -resourcegroupname $resourceGroup -hostpoo
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'));
  choco install fslogix -y;
  reg add HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles /v Enabled /t REG_DWORD /d 1; reg add HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles /v VHDLocations /t REG_MULTI_SZ /d \\gfsiwavduserprofilesa.file.core.windows.net\profiles;
- net use f: \\gfsiwavduserprofilesa.file.core.windows.net\profiles /user:Azure\gfsiwavduserprofilesa mubZzXU+mNyvwi99EbmNIsoM8UBfrP5yf7T/AH5dk23LzYW4Fpp7bfsDIGShJoz55Jm3QnKlthUg9GWeiOI8vA==;
+ net use f: \\gfsiwavduserprofilesa.file.core.windows.net\profiles /user:Azure\gfsiwavduserprofilesa key;
  icacls f: /grant "${assignedUser}:(M)"; icacls f: /grant '"Creator Owner":(OI)(CI)(IO)(M)'; icacls f: /remove "Authenticated Users"; icacls f: /remove "Builtin\\Users"
