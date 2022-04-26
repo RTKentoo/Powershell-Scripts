a110501b5cc21aa5849610d27eb71087b09396b0
Kent Taylor <r.t.kentoo@gmail.com>
2022-04-26 08:15:26 -0700

    Update azure_kv_permissions.ps1

--- azure_kv_permissions.ps1
+++ azure_kv_permissions.ps1
@@ -1,8 +1,8 @@
-$subscription = "80031227-6198-4a96-bf00-6efd88bfef19"
+$subscription = ""
 $keyVaults = Get-AzKeyVault #-SubscriptionId $subscription
 foreach ($vault in $keyVaults) {
     Set-AzKeyVaultAccessPolicy -VaultName $vault.vaultName `
-    -UserPrincipalName 'ktaylor-a@genesis-fs.com' `
+    -UserPrincipalName '' `
     -PermissionstoKeys Get, List, Update, Create, Import, Delete, Recover, Backup, Restore `
     -PermissionsToSecrets Get, List, Set, Delete, Recover, Backup, Restore `
     -PermissionsToCertificate Get, List, Update, Create, Import, Delete, Recover, Backup, Restore, ManageContacts, ManageIssuers, GetIssuers, ListIssuers, SetIssuers, DeleteIssuers 
