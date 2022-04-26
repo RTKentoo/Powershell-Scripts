38d014e385041e26166c87f5f44afd0d5faff4a7
Kent Taylor <r.t.kentoo@gmail.com>
2022-04-26 08:17:31 -0700

    Update azure_avd_vm_fslogix_install_configure.ps1

--- azure_avd_vm_fslogix_install_configure.ps1
+++ azure_avd_vm_fslogix_install_configure.ps1
@@ -1,5 +1,5 @@
-$hostpoolName = 'gfsi-w-avd-hp'
-$resourceGroup = 'gfsi-w-avd-rg'
+$hostpoolName = ''
+$resourceGroup = ''
 $vmName = [System.Net.Dns]::GetHostByName($env:computerName).hostname
 $assignedUser = (get-azwvdsessionhost -resourcegroupname $resourceGroup -hostpoolname $hostpoolName).assigneduser
 
@@ -7,6 +7,6 @@ $assignedUser = (get-azwvdsessionhost -resourcegroupname $resourceGroup -hostpoo
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'));
  choco install fslogix -y;
- reg add HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles /v Enabled /t REG_DWORD /d 1; reg add HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles /v VHDLocations /t REG_MULTI_SZ /d \\gfsiwavduserprofilesa.file.core.windows.net\profiles;
- net use f: \\gfsiwavduserprofilesa.file.core.windows.net\profiles /user:Azure\gfsiwavduserprofilesa key;
+ reg add HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles /v Enabled /t REG_DWORD /d 1; reg add HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles /v VHDLocations /t REG_MULTI_SZ /d \\storageaccount.file.core.windows.net\container;
+ net use f: \\storageaccount.file.core.windows.net\profiles /user:Azure\container key;
  icacls f: /grant "${assignedUser}:(M)"; icacls f: /grant '"Creator Owner":(OI)(CI)(IO)(M)'; icacls f: /remove "Authenticated Users"; icacls f: /remove "Builtin\\Users"
